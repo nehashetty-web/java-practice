a5a2a76c299d63352c86f010042013d2e8011aef
nehashetty-web <nehasree542@gmail.com>
2026-03-25 13:56:43 +0530

    Create day_of_week.java

---
+++ day_of_week.java
@@ -0,0 +1,23 @@
+ public class Main {
+    public static void main(String[] args) {
+        int day=3;
+        switch(day){
+        case 1 :
+             System.out.println("monday");
+             break;
+        case 2:
+            System.out.println("tuesday");
+            break;
+        case 3:
+            System.out.println("wednesday");
+            break;
+        case 4:
+            System.out.println("thursday");
+            break;
+        case 5 :
+            System.out.println("friday")
+        default:
+             System.out.println("other day ");
+        }
+    }
+ }
