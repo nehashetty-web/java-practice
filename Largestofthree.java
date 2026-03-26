dfd792ce274a1e17b875f32cc68ebfafc2582923
nehashetty-web <nehasree542@gmail.com>
2026-03-26 14:59:04 +0530

    Create Largestofthree.java

---
+++ Largestofthree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Largestofthree{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in); 
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int c = sc.nextInt();
+        int largest;
+        if(a>=b && a>=c){
+            largest =a;
+        }else if(b>=a && b>=c){
+            largest =b;
+        }else{
+            largest =c;
+        }
+        System.out.println("the largest is "+largest);
+        sc.close();
+
+        }
+        }
