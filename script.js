c257e103b2b31d938c25206b8fe81070da67ae49
nehashetty-web <nehasree542@gmail.com>
2026-04-14 23:07:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let name="neha"
+console.log(name)
+
+let a =7
+let b =9
+console.log(a+b)
+
+let username="neha"
+let age=20
+console.log(`my name is ${username} i am ${age} old`);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a % b); 
+
+let x = 5;
+console.log(x++); // 5 (first print, then increase)
+console.log(x);   // 6
+
+
+console.log("5" == 5);
+console.log("5" === 5);
